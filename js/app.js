35d53ce9c3b656ae93bc1da6b4d1c2fbafef4d76
Paul Guardino <paul@inverseparadox.net>
2016-10-30 20:44:31 -0400

    Price Table Styling and Mobile Menu Styling

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 (function($){
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
-});
+
+    $(document).ready( function(){
+        var lastItem = $('.mega-menu-item-57 .mega-sub-menu li:gt(5)');
+        $(lastItem).hide;
+    });
+
 })(jQuery);
