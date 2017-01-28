ac0cb34ff7bf5f9f3467d0447e3ea29651834f93
Paul Guardino <guardinopaul@gmail.com>
2017-01-28 12:50:52 -0500

    Twitter Feed Styles

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
     $(document).ready( function(){
         $(window).on('load',function(){
-            $('#cff.cff-fixed-height').mCustomScrollbar({
+            $('#cff.cff-fixed-height, .twitter-feed-wrap .ctf-type-usertimeline').mCustomScrollbar({
                 theme: 'corropolese'
             });
         });
