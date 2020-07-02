6020b6bc2c02c167c4e2186b27440d560250402f
Guardino, Paul <Guardino.Paul@pennmutual.com>
2020-07-02 01:26:09 -0400

    Facebook pixel code

--- header.php
+++ header.php
@@ -32,6 +32,16 @@
 
     <?php wp_head(); ?>
     <meta name="google-site-verification" content="t09mGUK7jhNbOke7huUOgEYrTvS-90h_tbom5_cw0Lo" />
+     <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1774517649433672');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1774517649433672&ev= PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
 </head>
 <body <?php body_class(); ?>>
     <div id="page-container">
